d5d49948315282eecabee719ee274c0114d3b3d8
Ereiarrus <ereiarrus@gmail.com>
2026-01-04 08:26:41 +0000

    adding types

--- converter.py
+++ converter.py
@@ -7,9 +7,10 @@ from fractions import Fraction
 import glob, shutil
 import hashlib
 import random
+from typing import Any, Dict, List, Tuple
 
 # Taken from https://stackoverflow.com/a/600612/119527
-def mkdir_p(path):
+def mkdir_p(path: str) -> None:
     try:
         os.makedirs(path)
     except OSError as exc:
@@ -17,7 +18,7 @@ def mkdir_p(path):
             pass
         else: raise
 
-def safe_open_w(path):
+def safe_open_w(path: os.PathLike[str] | str):
     ''' Open "path" for writing, creating any parent directories as needed.
     '''
     mkdir_p(os.path.dirname(path))
@@ -28,23 +29,30 @@ def seed_from_string(s: str) -> int:
     return int.from_bytes(digest[:8], "big")  # 64-bit seed
 
 
-bs_map_json = {}
-bs_info_json = {}
-at_map_json = {}
+bs_map_json: Dict[str, Any] = {}
+bs_info_json: Dict[str, Any] = {}
+at_map_json: Dict[str, Any] = {}
 
-base_directory = Path(__file__).parent
-bs_directory = base_directory / "examples" / "BS" / "4d2be (1-800 - Alice)"
-bs_map_file_path = bs_directory / "EasyStandard.dat"
-bs_info_file_path = bs_directory / "Info.dat"
+base_directory: Path = Path(__file__).parent
+bs_directory: Path = base_directory / "examples" / "BS" / "4d2be (1-800 - Alice)"
+bs_map_file_path: Path = bs_directory / "EasyStandard.dat"
+bs_info_file_path: Path = bs_directory / "Info.dat"
 
 with open(bs_map_file_path, 'r') as bs_map_file:
     bs_map_json = json.load(bs_map_file)
 with open(bs_info_file_path, 'r') as bs_info_file:
     bs_info_json = json.load(bs_info_file)
 
-at_song_file_name = bs_info_json["_songName"] + " - " + bs_info_json["_songAuthorName"] + " " + bs_info_json["_songSubName"] + ".ogg"
-beats_per_measure = 4  # TODO: un-hardcode?
-at_map_output_json = {
+at_song_file_name: str = (
+    bs_info_json["_songName"]
+    + " - "
+    + bs_info_json["_songAuthorName"]
+    + " "
+    + bs_info_json["_songSubName"]
+    + ".ogg"
+)
+beats_per_measure: int = 4  # TODO: un-hardcode?
+at_map_output_json: Dict[str, Any] = {
     "metadata": {
         "custom": True,
         "authorID": {
@@ -116,37 +124,37 @@ at_map_output_json["choreographies"]["list"][0]["header"] = {
                     "choreoType": 0
                 }
 at_map_output_json["choreographies"]["list"][0]["data"] = {"events": []}
-at_events = at_map_output_json["choreographies"]["list"][0]["data"]["events"]
+at_events: List[Dict[str, Any]] = at_map_output_json["choreographies"]["list"][0]["data"]["events"]
 
 
-song_file = bs_directory / bs_info_json["_songFilename"]
-song_length = OggVorbis(song_file).info.length
+song_file: Path = bs_directory / bs_info_json["_songFilename"]
+song_length: float = OggVorbis(song_file).info.length
 at_map_output_json["metadata"]["songEndTimeInSeconds"] = song_length  # TODO; also, can we just not have these numbers? would they be inferred?
 at_map_output_json["metadata"]["songFullLengthInSeconds"] = song_length  # TODO; also, can we just not have these numbers? would they be inferred?
 
-bs_x_range = 3  # left-most centre to right-most in-line
-bs_y_range = 2  # bottom-most centre to top-most centre in-line
-at_x_range = 2.5  # TODO: player customisable
-at_y_range = 1.5  # TODO: player customisable
-at_y_min = 0.3  # TODO: player customisable
-x_scale = at_x_range / bs_x_range
-y_scale = at_y_range / bs_y_range
-x_wobble_factor = 0.1  # TODO: player customisable
-y_wobble_factor = 0.1  # TODO: player customisable
-x_wobble = x_scale * x_wobble_factor
-y_wobble = y_scale * y_wobble_factor
-offsets = [  # remember the y axis is flipped here - top is at bottom, bottom is on top
+bs_x_range: float = 3  # left-most centre to right-most in-line; TODO: needs to change for maps with >4 lanes
+bs_y_range: float = 2  # bottom-most centre to top-most centre in-line
+at_x_range: float = 2.5  # TODO: player customisable
+at_y_range: float = 1.5  # TODO: player customisable
+at_y_min: float = 0.3  # TODO: player customisable
+x_scale: float = at_x_range / bs_x_range
+y_scale: float = at_y_range / bs_y_range
+x_wobble_factor: float = 0.1  # TODO: player customisable
+y_wobble_factor: float = 0.1  # TODO: player customisable
+x_wobble: float = x_scale * x_wobble_factor
+y_wobble: float = y_scale * y_wobble_factor
+positions: List[List[Tuple[float, float]]] = [  # remember the y axis is flipped here - top is at bottom, bottom is on top; TODO: this needs to be bigger for maps with >4 lanes
     [(-x_scale*3/2, at_y_min + y_scale/2),   (-x_scale/2, at_y_min + y_scale/2),   (x_scale/2, at_y_min + y_scale/2),   (x_scale*3/2, at_y_min + y_scale/2)],
     [(-x_scale*3/2, at_y_min + y_scale*3/2), (-x_scale/2, at_y_min + y_scale*3/2), (x_scale/2, at_y_min + y_scale*3/2), (x_scale*3/2, at_y_min + y_scale*3/2)],
     [(-x_scale*3/2, at_y_min + y_scale*5/2), (-x_scale/2, at_y_min + y_scale*5/2), (x_scale/2, at_y_min + y_scale*5/2), (x_scale*3/2, at_y_min + y_scale*5/2)],
 ]
 random.seed(seed_from_string(at_song_file_name))
 for e in bs_map_json["colorNotes"]:
-    beat = e["b"]
-    f = Fraction(beat).limit_denominator(beats_per_measure)
-    whole_beat = f.numerator // f.denominator
-    remainder = f - whole_beat
-    x_pos, y_pos = offsets[e["y"]][e["x"]]  # TODO: do we want to add a randomness factor to this? just so they don't all look exactly the same positioned
+    beat: float = e["b"]
+    f: Fraction = Fraction(beat).limit_denominator(beats_per_measure)
+    whole_beat: int = f.numerator // f.denominator
+    remainder: Fraction = f - whole_beat
+    x_pos, y_pos = positions[e["y"]][e["x"]]  # TODO: do we want to add a randomness factor to this? just so they don't all look exactly the same positioned
     x_pos += x_wobble * (random.random() * 2 - 1)
     y_pos += y_wobble * (random.random() * 2 - 1)
     at_events.append(
@@ -185,9 +193,8 @@ https://bsmg.wiki/mapping/map-format/beatmap.html explains the meanings:
 
 
 # Save outputs
-output_dir = base_directory / "output"
+output_dir: Path = base_directory / "output"
 with safe_open_w(output_dir / (bs_info_json["_songAuthorName"] + " - " + bs_info_json["_songName"] + " " + bs_info_json["_songSubName"] + " - " + bs_info_json["_levelAuthorName"] + ".ats")) as output_file:
     output_file.write(json.dumps(at_map_output_json))
 
 shutil.copy2(song_file, output_dir / at_song_file_name)
-
