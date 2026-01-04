df9fc097b061023a8378903ab045b90eb98968a3
Ereiarrus <ereiarrus@gmail.com>
2026-01-04 08:03:13 +0000

    tool makes maps, but their notes start a bit early, and are just simple gems

--- converter.py
+++ converter.py
@@ -1,7 +1,9 @@
 import json
+from mutagen.oggvorbis import OggVorbis
 from pathlib import Path
 import os, os.path
 import errno
+from fractions import Fraction
 import glob, shutil
 
 # Taken from https://stackoverflow.com/a/600612/119527
@@ -29,16 +31,148 @@ base_directory = Path(__file__).parent
 bs_directory = base_directory / "examples" / "BS" / "4d2be (1-800 - Alice)"
 bs_map_file_path = bs_directory / "EasyStandard.dat"
 bs_info_file_path = bs_directory / "Info.dat"
-# at_map_file_path = base_directory / "examples" / "AT" / "Yakuza 0 • Friday Night - Jenolin" / "Yakuza 0 - Friday Night - Jenolin.ats"
 
 with open(bs_map_file_path, 'r') as bs_map_file:
     bs_map_json = json.load(bs_map_file)
 with open(bs_info_file_path, 'r') as bs_info_file:
     bs_info_json = json.load(bs_info_file)
-# with open(at_map_file_path, 'r') as at_map_file:
-#     at_map_json = json.load(at_map_file)
 
-at_map_output_json = {}
+at_song_file_name = bs_info_json["_songName"] + " - " + bs_info_json["_songAuthorName"] + " " + bs_info_json["_songSubName"] + ".ogg"
+beats_per_measure = 4  # TODO: un-hardcode?
+at_map_output_json = {
+    "metadata": {
+        "custom": True,
+        "authorID": {
+            "platformID": "OC",
+            "displayName": bs_info_json["_levelAuthorName"],
+        },
+        "songID": "BEAT SABER IMPORT",
+        "title": bs_info_json["_songName"],
+        "artist": bs_info_json["_songAuthorName"],
+        "koreography": {
+            "m_FileID": 0,
+            "m_PathID": 0
+        },
+        "descriptor": "",
+        "sceneName": "Universal",
+        "avgBPM": bs_info_json["_beatsPerMinute"],
+        "tempoSections": [{
+                "startTimeInSeconds": 4.0,
+                "beatsPerMeasure": beats_per_measure,
+                "beatsPerMinute": bs_info_json["_beatsPerMinute"],
+                "doesStartNewMeasure": False
+            }, {
+                "startTimeInSeconds": 4.001,
+                "beatsPerMeasure": beats_per_measure,
+                "beatsPerMinute": bs_info_json["_beatsPerMinute"],
+                "doesStartNewMeasure": True
+            }
+        ],
+        "songEventTracks": [],
+        "songFilename": at_song_file_name,
+        "firstBeatTimeInSeconds": 0.0,
+        "songShortStartTimeInSeconds": 0,
+        "songShortStopTimeInSeconds": 0,
+        "songShortLengthInSeconds": 0,
+        "songStartFadeTime": 0,
+        "songEndFadeTime": 0,
+        "previewStartInSeconds": 30.0,
+        "previewDurationInSeconds": 10.0,
+        "songStartBufferInSeconds": 0.0,
+        "choreoJSONs": [],
+        "animClips": [],
+        "speed": 0.0,
+        "quantizeSize": 0.10000000149011612,
+        "includeInArcades": True,
+        "supportedModalitySets": 2,
+        "drumMedSFX": "",
+        "drumMaxSFX": ""
+    }
+}
+at_map_output_json["choreographies"] = {}
+at_map_output_json["choreographies"]["list"] = []
+at_map_output_json["choreographies"]["list"].append({})
+at_map_output_json["choreographies"]["list"][0]["header"] = {
+                    "id": "cust_beat_saber_map",
+                    "descriptor": "",
+                    "name": "Easy",  # TODO: this is the difficulty name; take it from the filename OR Info.dat on beat saber
+                    "metadata": "",
+                    "spawnAheadTime": {  # TODO: is the the JD of the map?
+                        "beat": 8,
+                        "numerator": 0,
+                        "denominator": 1
+                    },
+                    "gemSpeed": 18.0,  # TODO: should probably grab from Info.dat - "_noteJumpMovementSpeed"
+                    "gemRadius": 1.0,
+                    "handRadius": 0.27000001072883608,
+                    "animClipPath": "",
+                    "buildVersion": "",
+                    "requiredModalities": 2,
+                    "choreoType": 0
+                }
+at_map_output_json["choreographies"]["list"][0]["data"] = {"events": []}
+at_events = at_map_output_json["choreographies"]["list"][0]["data"]["events"]
+
+
+song_file = bs_directory / bs_info_json["_songFilename"]
+song_length = OggVorbis(song_file).info.length
+at_map_output_json["metadata"]["songEndTimeInSeconds"] = song_length  # TODO; also, can we just not have these numbers? would they be inferred?
+at_map_output_json["metadata"]["songFullLengthInSeconds"] = song_length  # TODO; also, can we just not have these numbers? would they be inferred?
+
+bs_x_range = 3  # left-most centre to right-most in-line
+bs_y_range = 2  # bottom-most centre to top-most centre in-line
+at_x_range = 2.5  # TODO: player customisable
+at_y_range = 1.5  # TODO: player customisable
+at_y_min = 0.3  # TODO: player customisable
+x_scale = at_x_range / bs_x_range
+y_scale = at_y_range / bs_y_range
+offsets = [  # remember the y axis is flipped here - top is at bottom, bottom is on top
+    [(-x_scale*3/2, at_y_min + y_scale/2),   (-x_scale/2, at_y_min + y_scale/2),   (x_scale/2, at_y_min + y_scale/2),   (x_scale*3/2, at_y_min + y_scale/2)],
+    [(-x_scale*3/2, at_y_min + y_scale*3/2), (-x_scale/2, at_y_min + y_scale*3/2), (x_scale/2, at_y_min + y_scale*3/2), (x_scale*3/2, at_y_min + y_scale*3/2)],
+    [(-x_scale*3/2, at_y_min + y_scale*5/2), (-x_scale/2, at_y_min + y_scale*5/2), (x_scale/2, at_y_min + y_scale*5/2), (x_scale*3/2, at_y_min + y_scale*5/2)],
+]
+
+for e in bs_map_json["colorNotes"]:
+    beat = e["b"]
+    f = Fraction(beat).limit_denominator(beats_per_measure)
+    whole_beat = f.numerator // f.denominator
+    remainder = f - whole_beat
+    # print(e["x"], e["y"])
+    x_offset, y_offset = offsets[e["y"]][e["x"]]  # TODO: do we want to add a randomness factor to this? just so they don't all look exactly the same positioned
+    # print(x_offset, y_offset)
+    at_events.append(
+        {
+            "type": (1 if e["c"] == 0 else 2),  # TODO
+            "hasGuide": False,
+            "time": {
+                "beat": whole_beat,
+                "numerator": remainder.numerator,
+                "denominator": remainder.denominator
+            },
+            "beatDivision": 2,
+            "position": {
+                "x": x_offset,
+                "y": y_offset,
+                "z": 0.0
+            },
+            "subPositions": [],
+            "broadcastEventID": 0
+        }
+    )
+    # print(at_events)
+
+
+"""
+https://bsmg.wiki/mapping/map-format/beatmap.html explains the meanings:
+{
+    "b": 5,
+    "x": 2,
+    "y": 0,
+    "a": 0,
+    "c": 1,
+    "d": 1 
+}
+"""
 
 
 # Save outputs
@@ -46,8 +180,5 @@ output_dir = base_directory / "output"
 with safe_open_w(output_dir / (bs_info_json["_songAuthorName"] + " - " + bs_info_json["_songName"] + " " + bs_info_json["_songSubName"] + " - " + bs_info_json["_levelAuthorName"] + ".ats")) as output_file:
     output_file.write(json.dumps(at_map_output_json))
 
-shutil.copy2(
-        bs_directory / bs_info_json["_songFilename"], 
-        output_dir / (bs_info_json["_songName"] + " - " + bs_info_json["_songAuthorName"] + " " + bs_info_json["_songSubName"] + ".ogg")
-)
+shutil.copy2(song_file, output_dir / at_song_file_name)
 
